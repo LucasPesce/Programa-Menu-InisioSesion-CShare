cb287a7a90d0cddc6311d5011a223280d4d0e100
Lucas Pesce <117691263+LucasPesce@users.noreply.github.com>
2023-08-01 11:01:42 -0300

    Add files via upload

---
+++ Menu-InicioDeSecion/Ejercicio 1/Login.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Ejercicio_1
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textUsuario = new System.Windows.Forms.TextBox();
+            this.textContraseña = new System.Windows.Forms.TextBox();
+            this.comboModulo = new System.Windows.Forms.ComboBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(74, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(74, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contraseña";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(74, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Modulo";
+            // 
+            // textUsuario
+            // 
+            this.textUsuario.ForeColor = System.Drawing.Color.Blue;
+            this.textUsuario.Location = new System.Drawing.Point(195, 47);
+            this.textUsuario.MaxLength = 10;
+            this.textUsuario.Name = "textUsuario";
+            this.textUsuario.Size = new System.Drawing.Size(220, 20);
+            this.textUsuario.TabIndex = 3;
+            this.textUsuario.TextChanged += new System.EventHandler(this.textUsuario_TextChanged);
+            // 
+            // textContraseña
+            // 
+            this.textContraseña.Location = new System.Drawing.Point(195, 94);
+            this.textContraseña.MaxLength = 10;
+            this.textContraseña.Name = "textContraseña";
+            this.textContraseña.PasswordChar = '#';
+            this.textContraseña.Size = new System.Drawing.Size(220, 20);
+            this.textContraseña.TabIndex = 4;
+            this.textContraseña.TextChanged += new System.EventHandler(this.textContraseña_TextChanged);
+            // 
+            // comboModulo
+            // 
+            this.comboModulo.FormattingEnabled = true;
+            this.comboModulo.Location = new System.Drawing.Point(195, 143);
+            this.comboModulo.Name = "comboModulo";
+            this.comboModulo.Size = new System.Drawing.Size(220, 21);
+            this.comboModulo.TabIndex = 5;
+            this.comboModulo.SelectedIndexChanged += new System.EventHandler(this.comboModulo_SelectedIndexChanged);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(445, 47);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 6;
+            this.btnAceptar.Text = "&Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(445, 97);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 7;
+            this.btnCancelar.Text = "&Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 220);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.comboModulo);
+            this.Controls.Add(this.textContraseña);
+            this.Controls.Add(this.textUsuario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textUsuario;
+        private System.Windows.Forms.TextBox textContraseña;
+        private System.Windows.Forms.ComboBox comboModulo;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
+
---
+++ Menu-InicioDeSecion/Ejercicio 1/Login.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_1
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        // INICIALIZAMOS LOS ELEMENTOS DEL FORMULARIO
+        private void Login_Load(object sender, EventArgs e)             
+        {
+            textUsuario.Clear();            //Limpia la caja de texto del Usuario
+            textContraseña.Clear();         //Limpia la caja de texto de la Contraseña
+
+            comboModulo.Items.Clear();      //Limpia las opciones de Modulo
+            comboModulo.Items.Add("ADM");   //Agrega opcion al modulo
+            comboModulo.Items.Add("SIST");
+            comboModulo.Items.Add("COM");
+            comboModulo.Items.Add("VTA");
+
+            textContraseña.Enabled = false; //Ihnabilita la caja de texto de contraseña
+            comboModulo.Enabled = false;    //Ihnabilita los modulos
+            btnAceptar.Enabled = false;     //Ihnabilita el boton de aceptar
+        }
+
+        //ACTIVACION PROGRESIVA DE LOS CAMPOS A LLENAR
+        private void comboModulo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboModulo.Text != null)           //Si la caja de texto de Usuario no esta vacia, se activara el campo contraseña
+            {
+                btnAceptar.Enabled = true;
+            }
+        }
+        private void textContraseña_TextChanged(object sender, EventArgs e)
+        {
+            if (textContraseña.Text != null)           //Si la caja de texto de Contraseña no esta vacia, se activara el campo de los modulos
+            {
+                comboModulo.Enabled = true;
+            }
+        }
+        private void textUsuario_TextChanged(object sender, EventArgs e)
+        {
+            if (textUsuario.Text != null)           //Si la caja de texto de Usuario no esta vacia, se activara el campo contraseña
+            {
+                textContraseña.Enabled = true;
+            }
+
+        }
+
+        //BOTENES DEL FORMULARIO
+        private void btnCancelar_Click(object sender, EventArgs e)  //BOTON CANCELAR
+        {
+            this.Close();
+        }
+
+        int intentos = 0;
+        private void btnAceptar_Click(object sender, EventArgs e)  //BOTON ACEPTAR
+        {
+            if ((textUsuario.Text == "Adm" && textContraseña.Text == "adm123" &&                                 
+                (comboModulo.Text == "ADM" || comboModulo.Text == "COM" || comboModulo.Text == "VTA")) ||
+
+                (textUsuario.Text == "Jhon" && textContraseña.Text == "1234" && comboModulo.Text == "SIST") ||  
+
+                (textUsuario.Text == "Ceci" && textContraseña.Text == "1234" && (comboModulo.Text == "ADM" || comboModulo.Text == "VTA")) ||
+
+                (textUsuario.Text == "God" && textContraseña.Text == "god123" &&
+                (comboModulo.Text == "ADM" || comboModulo.Text == "COM" || comboModulo.Text == "VTA" || comboModulo.Text == "SIST"))) 
+            {
+                this.Hide();                                // Ocultar Formulario
+                Bienvenido bienvenido = new Bienvenido();   // Crear Formulario de bienvenida
+                bienvenido.ShowDialog();
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show("Error en los datos ingresados");
+                intentos++;
+                if (intentos == 2)
+                {
+                    this.Close();
+                }
+            }
+        }
+    }
+}
